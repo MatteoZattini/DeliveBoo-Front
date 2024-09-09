c35157e947fef9695a77d4f4b215e1372378bddf
damirusso95 <damirusso95@gmail.com>
2024-09-09 10:54:40 +0200

    modifiche per slug front

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,75 +1,3 @@
-<script>
-import axios from 'axios';
-import TypologyCard from './TypologyCard.vue';
-import RestaurantCard from './RestaurantCard.vue';
-import skeleton from 'primevue/skeleton';
-
-
-export default {
-    name: 'AppHome',
-
-    components: {
-        TypologyCard,
-        RestaurantCard,
-        skeleton,
-    },
-
-    data() {
-        return {
-            categories: '',
-            categoriesSelected: [],
-            restaurants: '',
-            isLoading: true,
-            notFound: false
-        }
-    },
-
-    methods: {
-        getRestaurants() {
-            axios.get('http://localhost:8000/api/restaurants', { params: { categories: this.categoriesSelected } }).then((response) => {
-                console.log(response.data)
-                this.restaurants = response.data;
-                if (!response.data[0]) {
-                    this.notFound = true;
-                }else{
-                    this.notFound = false;
-                }
-            })
-        },
-
-        getCategory(name) {
-
-            if (this.categoriesSelected.includes(name)) {
-                let x = this.categoriesSelected.indexOf(name)
-                this.categoriesSelected.splice(x, 1)
-                console.log(this.categoriesSelected);
-            } else {
-                console.log("cliccato", name)
-                this.categoriesSelected.push(name)
-                console.log(this.categoriesSelected)
-            }
-        },
-
-        getSingleRestaurant(id) {
-            router.push({
-                name: 'single-restaurant',
-                params: { id: id }
-            })
-        }
-
-    },
-
-    mounted() {
-        axios.get('http://localhost:8000/api/categories').then(response => {
-            console.log(response);
-            this.categories = response.data;
-            console.log(this.categories)
-            this.isLoading = false
-        })
-    }
-}
-</script>
-
 <template>
     <main>
         <div class="container">
@@ -89,32 +17,30 @@ export default {
                             <skeleton shape="circle" size="7rem" class="mr-2"></skeleton>
                         </div>
 
-                        <template v-for="item in categories">
+                        <template v-for="item in categories" :key="item.id">
                             <div class="d-flex flex-column align-items-center p-5">
-                                <TypologyCard @selected="getCategory(item.name), getRestaurants()" :name="item.name" />
+                                <TypologyCard 
+                                    @selected="getCategory(item.name), getRestaurants()" 
+                                    :name="item.name" 
+                                />
                             </div>
                         </template>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- <div class="container">
-            <div class="row">
-                <div class="col-12 d-flex justify-content-center">
-                    <button @click="getRestaurants()" class="btn btn-primary">Invia</button>
-                </div>
-            </div>
-        </div> -->
 
         <div class="container">
             <div class="row">
                 <div class="col-12 d-flex justify-content-center flex-wrap">
                     <div v-if="restaurants.length > 0">
-                        <template v-for="restaurant in restaurants">
-
-                            <router-link class="btn" :to="{ name: 'single-restaurant', params: { id: restaurant.id } }">
-                                <RestaurantCard  @click="getSingleRestaurant(restaurant.id)"
-                                     :imageSrc="restaurant.img" :restaurantName="restaurant.name" :restaurant="restaurant"/>
+                        <template v-for="restaurant in restaurants" :key="restaurant.id">
+                            <router-link class="btn" :to="{ name: 'single-restaurant', params: { slug: restaurant.slug } }">
+                                <RestaurantCard  
+                                    :imageSrc="getImageSrc(restaurant.img)" 
+                                    :restaurantName="restaurant.name" 
+                                    :restaurant="restaurant"
+                                />
                             </router-link>
                         </template>
                     </div>
@@ -123,13 +49,76 @@ export default {
                             La richiesta fatta da costei non ha prodotto alcun risultato
                         </h2>
                     </div>
-
                 </div>
             </div>
         </div>
-
     </main>
 </template>
+
+<script>
+import axios from 'axios';
+import TypologyCard from './TypologyCard.vue';
+import RestaurantCard from './RestaurantCard.vue';
+import skeleton from 'primevue/skeleton';
+
+export default {
+    name: 'AppHome',
+    components: {
+        TypologyCard,
+        RestaurantCard,
+        skeleton,
+    },
+    data() {
+        return {
+            categories: [],
+            categoriesSelected: [],
+            restaurants: [],
+            isLoading: true,
+            notFound: false
+        };
+    },
+    methods: {
+        async getRestaurants() {
+            try {
+                const response = await axios.get('http://localhost:8000/api/restaurants', {
+                    params: { categories: this.categoriesSelected }
+                });
+                this.restaurants = response.data;
+                this.notFound = response.data.length === 0;
+            } catch (error) {
+                console.error('Error fetching restaurants:', error);
+            }
+        },
+        async getCategory(name) {
+            if (this.categoriesSelected.includes(name)) {
+                const index = this.categoriesSelected.indexOf(name);
+                this.categoriesSelected.splice(index, 1);
+            } else {
+                this.categoriesSelected.push(name);
+            }
+            await this.getRestaurants();
+        },
+        getImageSrc(img) {
+            return img.startsWith('http') ? img : `/storage/${img}`;
+        }
+    },
+    async mounted() {
+        try {
+            const response = await axios.get('http://localhost:8000/api/categories');
+            this.categories = response.data;
+        } catch (error) {
+            console.error('Error fetching categories:', error);
+        } finally {
+            this.isLoading = false;
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* Aggiungi i tuoi stili qui */
+</style>
+
 
 <style scoped>
 /* .p-inputtext {
--- src/components/AppSingleRestaurant.vue
+++ src/components/AppSingleRestaurant.vue
@@ -1,55 +1,47 @@
+
 <script>
 import axios from 'axios';
 
 export default {
     name: 'AppSingleRestaurant',
-
-    components: {
-
-    },
-
     data() {
         return {
-            restaurant: '',
+            restaurant: null,
+        };
+    },
+    methods: {
+        getImageSrc(img) {
+            return img.startsWith('http') ? img : `/storage/${img}`;
         }
     },
-
-    methods: {
-
-    },
-
-    mounted() {
-        axios.get('http://localhost:8000/api/restaurants/' + this.$route.params.id).then((response) => {
-            console.log(response);
+    async mounted() {
+        try {
+            const slug = this.$route.params.slug;
+            const response = await axios.get(`http://localhost:8000/api/restaurants?slug=${slug}`);
             this.restaurant = response.data;
-            console.log(this.restaurant);
-        });
+        } catch (error) {
+            console.error('Error fetching restaurant details:', error);
+        }
     }
 }
 </script>
-
 <template>
-
-    <div class="menu-section">
+    <div class="menu-section" v-if="restaurant">
         <div class="title-section d-flex flex-column">
-            <h1>{{ this.restaurant.name }}</h1>
+            <h1>{{ restaurant.name }}</h1>
             <h2>Menù</h2>
         </div>
         <div class="menu-items">
-            <div class="menu-item" v-for="dish in this.restaurant.dishes">
+            <div class="menu-item" v-for="dish in restaurant.dishes" :key="dish.id">
                 <h3>{{ dish.name }}</h3>
                 <p>{{ dish.description }}</p>
                 <span class="price">{{ dish.price }} €</span> 
-                <img :src="dish.img" :alt="dish.name" />
+                <img :src="getImageSrc(dish.img)" :alt="dish.name" />
             </div>
         </div>
-        
     </div>
-
-
-
-
 </template>
+
 
 <style scoped>
 .menu-section {
